ae99ccb1c036806e7c83175b3432198f562af33a
victor <viccel2007@gmail.com>
2023-03-10 10:04:57 -0600

    report controller

--- demo-neoris/src/main/java/com/apineoris/demoneoris/controller/MovimientoController.java
+++ demo-neoris/src/main/java/com/apineoris/demoneoris/controller/MovimientoController.java
@@ -4,9 +4,15 @@ import com.apineoris.demoneoris.dto.MovimientoDto;
 import com.apineoris.demoneoris.entity.Movimiento;
 import com.apineoris.demoneoris.services.MovimientoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
+import javax.websocket.server.PathParam;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -41,8 +47,15 @@ public class MovimientoController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public MovimientoDto deleteById(@PathVariable(name = "id") long id){
+    public MovimientoDto deleteById(@PathVariable(name = "id") long id) {
         return service.deleteMovimientoById(id);
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<List<MovimientoDto>> getReporte(@RequestParam(value = "inicio") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate inicio,
+                                                          @RequestParam(value = "fin") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) final LocalDate fin) {
+        List<MovimientoDto> reporte = service.getReporte(inicio, fin);
+        return ResponseEntity.ok(reporte);
     }
 
 }
